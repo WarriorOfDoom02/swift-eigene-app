5e0019dfea90264c48679f8faa8b2b5a8e5b2450
WarriorOfDoom02 <dominikmueller@aol.com>
2022-08-31 13:16:19 +0200

    Initial Commit

---
+++ EigeneApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EigeneApp
+//
+//  Created by Dominik Müller on 31.08.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EigeneApp/EigeneAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EigeneAppApp.swift
+//  EigeneApp
+//
+//  Created by Dominik Müller on 31.08.22.
+//
+
+import SwiftUI
+
+@main
+struct EigeneAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
